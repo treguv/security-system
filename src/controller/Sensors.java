41660f6281b52f707556957d88cc9f623b33b286
origamisith <lsedillo@uw.edu>
2021-06-10 17:23:43 -0700

    Very broken, don't

--- src/controller/Sensors.java
+++ src/controller/Sensors.java
@@ -4,10 +4,14 @@ import javafx.fxml.FXML;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
+/**
+ * This class constructs the
+ */
 public class Sensors{
 
     @FXML
@@ -16,6 +20,16 @@ public class Sensors{
     @FXML
     private void initialize(){
         int i = 0;
+        ColumnConstraints column0 = new ColumnConstraints();
+        column0.setPercentWidth(40);
+        column0.setHgrow();
+        ColumnConstraints column1 = new ColumnConstraints();
+        column1.setPercentWidth(5);
+        ColumnConstraints column2 = new ColumnConstraints();
+        column2.setPercentWidth(40);
+        ColumnConstraints column3 = new ColumnConstraints();
+        column3.setPercentWidth(5);
+        grid.setMinWidth(500);
         for(String s: new String[]{"cctv", "doorbell", "doorsensor", "motion"}) {
             try {
                 ImageView image = new ImageView();
@@ -23,11 +37,12 @@ public class Sensors{
                 ColorAdjust whiteOut = new ColorAdjust();
                 whiteOut.setBrightness(1.0);
                 image.setEffect(whiteOut);
-                image.setFitHeight(100);
                 image.setFitWidth(100);
-                grid.add(image, i%10, i/10,4,1);
-                i+=4;
-                grid.add(new Circle(4, Color.GREEN),i%10,i/10,1,1);
+                image.setFitHeight(100);
+
+                grid.add(image, i%4, i/4,1,1);
+                i++;
+                grid.add(new Circle(4, Color.GREEN),i%4,i/4,1,1);
                 i++;
             } catch (Exception e) {
                 System.out.println("couldn't bruh");
